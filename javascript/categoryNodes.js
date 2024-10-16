5245af2cc1a82264c2bef4a0aae0ad85286dc50d
wilsonlargo <136187461+wilsonlargo@users.noreply.github.com>
2024-10-16 14:58:30 -0500

    update

---
+++ javascript/categoryNodes.js
@@ -0,0 +1,47 @@
+﻿// You can find instructions for this file here:
+// http://www.treeview.net
+
+// Decide if the names are links or just the icons
+USETEXTLINKS = 1  //replace 0 with 1 for hyperlinks
+
+// Don't use icons
+USEICONS = 0
+
+// Decide if the tree is to start all open or just showing the root folders
+STARTALLOPEN = 0 //replace 0 with 1 to show the whole tree
+
+ICONPATH = '../images/' //change if the gif's folder is a subfolder, for example: 'images/'
+
+foldersTree = gFld("<i>Categories</i>", "")
+  aux1 = insFld(foldersTree, gFld("Adjetivos", "c01.htm"))
+  aux1 = insFld(foldersTree, gFld("Adverbios", "c02.htm"))
+  aux1 = insFld(foldersTree, gFld("Alimentos", "c03.htm"))
+  aux1 = insFld(foldersTree, gFld("Anfibios", "c04.htm"))
+  aux1 = insFld(foldersTree, gFld("Animales", "c05.htm"))
+  aux1 = insFld(foldersTree, gFld("Arboles", "c06.htm"))
+  aux1 = insFld(foldersTree, gFld("Aves", "c07.htm"))
+  aux1 = insFld(foldersTree, gFld("Colores", "c08.htm"))
+  aux1 = insFld(foldersTree, gFld("Conjunciones", "c09.htm"))
+  aux1 = insFld(foldersTree, gFld("Frases", "c10.htm"))
+  aux1 = insFld(foldersTree, gFld("Frutas", "c11.htm"))
+  aux1 = insFld(foldersTree, gFld("Herramientas", "c12.htm"))
+  aux1 = insFld(foldersTree, gFld("Insectos", "c13.htm"))
+  aux1 = insFld(foldersTree, gFld("Instrumentos", "c14.htm"))
+  aux1 = insFld(foldersTree, gFld("Mamiferos", "c15.htm"))
+  aux1 = insFld(foldersTree, gFld("Mitologia", "c16.htm"))
+  aux1 = insFld(foldersTree, gFld("Naturaleza", "c17.htm"))
+  aux1 = insFld(foldersTree, gFld("Nombres", "c18.htm"))
+  aux1 = insFld(foldersTree, gFld("Numeros", "c19.htm"))
+  aux1 = insFld(foldersTree, gFld("Parentesco", "c20.htm"))
+  aux1 = insFld(foldersTree, gFld("Partes del cuerpo", "c21.htm"))
+  aux1 = insFld(foldersTree, gFld("Peces", "c22.htm"))
+  aux1 = insFld(foldersTree, gFld("Plantas", "c23.htm"))
+  aux1 = insFld(foldersTree, gFld("Prefijos", "c24.htm"))
+  aux1 = insFld(foldersTree, gFld("Prefijos verbales", "c25.htm"))
+  aux1 = insFld(foldersTree, gFld("Pronombres", "c26.htm"))
+  aux1 = insFld(foldersTree, gFld("Reptiles", "c27.htm"))
+  aux1 = insFld(foldersTree, gFld("Salud", "c28.htm"))
+  aux1 = insFld(foldersTree, gFld("Saludos", "c29.htm"))
+  aux1 = insFld(foldersTree, gFld("Sufijos", "c30.htm"))
+  aux1 = insFld(foldersTree, gFld("Tiempos", "c31.htm"))
+  aux1 = insFld(foldersTree, gFld("Verbos", "c32.htm"))
